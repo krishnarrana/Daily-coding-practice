827eb171b5ffc5c06539ec4db0be774695234a18
Krishna <krishna2121.rana@gmail.com>
2019-04-02 19:45:46 +0200

    javascript event

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import PasswordRevealer from "./PasswordRevealer";
+import Mailto from "./MailTo";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <PasswordRevealer />
+        <Mailto email="foo@bar.baz" subject="Hello" body="Hello world!">
+          Mail me!
+        </Mailto>
       </div>
     );
   }
